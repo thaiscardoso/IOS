3afa65eb0fdddd8a4a762b014b0e4f1bfaccafe0
ios2686 <ios2686@Mac-mini-de-Caelum-36.local>
2012-05-07 21:07:37 -0300

    Initial Commit

---
+++ ContaChopp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ContaChopp
+//
+//  Created by ios2686 on 07/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ContaChopp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ContaChopp
+//
+//  Created by ios2686 on 07/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
